7391a3f3e17743e5a523669db50649332598bd22
Justin Chau <chausicle@gmail.com>
2018-08-08 15:20:18 -0700

    Implement capitalize function

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,14 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const strArray = []
+
+  for (let word of str.split(" ")) {
+    strArray.push(word[0].toUpperCase() + word.slice(1))
+  }
+
+  return strArray.join(" ")
+}
 
 module.exports = capitalize;
