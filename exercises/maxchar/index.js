4d9c41bafb6331859ce609a5ca1ea005f49dbae4
Justin Chau <chausicle@gmail.com>
2018-08-08 15:21:13 -0700

    Implement maxchar function

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const objectOfChar = {}
+  let maxCharacter = ""
+  let maxNum = 0
+
+  for (let char of str) {
+    // if object does not have own property of char, set key to char and value to 1
+    // otherwise increment value
+    if (!objectOfChar.hasOwnProperty(char)) objectOfChar[char] = 1
+    else objectOfChar[char]++
+
+    // check the value of the char if it's bigger than maxNum
+    // set the new maxNum and maxCharacter
+    if (objectOfChar[char] > maxNum) {
+      maxNum = objectOfChar[char]
+      maxCharacter = char
+    }
+  }
+
+  return maxCharacter
+}
 
 module.exports = maxChar;
