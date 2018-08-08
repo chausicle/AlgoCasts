4f5974176c37da9ba48df8723b6cfe2c2300733f
Justin Chau <chausicle@gmail.com>
2018-08-08 15:30:10 -0700

    Implement steps function

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,16 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let i = 1; i <= n; i++) {
+    let log = ""
+
+    for (let j = 1; j <= n; j++) {
+      if (j <= i) log += "#"
+      else log += " "
+    }
+    console.log(log);
+  }
+}
 
 module.exports = steps;
