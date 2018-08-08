013bc5000b0db8c92804e9252ab40480e17661aa
Justin Chau <chausicle@gmail.com>
2018-08-08 16:56:19 -0700

    Implement pyramid function

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,28 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  let spaces = n - 1
+  let sharps = 1
+  let log = ""
+  let columns
+
+  for (let i = 0; i < n; i++) {
+    if (i === 0) columns = 1
+    else columns += 2
+  }
+
+  for (let row = 0; row < n; row++) {
+    for (let col = 0; col < spaces; col++) log += " "
+    for (let col = 0; col < sharps; col++) log += "#"
+    for (let col = 0; col < spaces; col++) log += " "
+
+    console.log(log);
+
+    log = ""
+    spaces--
+    sharps += 2
+  }
+}
 
 module.exports = pyramid;
